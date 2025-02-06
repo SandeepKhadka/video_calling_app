42a0dcd6416f73169e397c2e5bb41badee3d7f99
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2025-02-06 21:08:07 +0545

    setting up project

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules', 'build', 'dist', '.expo'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
