af16fc17028510b52cfa31d1f7f1da6b73e37b26
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2025-02-06 21:10:31 +0545

    successfully added husky, lint and prettier

---
+++ app/(call)/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const CallLayout = () => {
+  return (
+    <View>
+      <Text>CallLayout</Text>
+    </View>
+  );
+};
+
+export default CallLayout;
+
+const styles = StyleSheet.create({});
