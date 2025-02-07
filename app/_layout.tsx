f72196f194ae9148636a98c1ef66d9e413fbb259
Sandeep Khadka <sandeepkhadka4935g@gmail.com>
2025-02-07 11:37:17 +0545

    integrating clerk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,49 @@
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo';
+import { TokenCache } from '@clerk/clerk-expo/dist/cache/types';
 import { Slot } from 'expo-router';
-import { SafeAreaView } from 'react-native';
-
 import 'react-native-reanimated';
+import * as SecureStore from 'expo-secure-store';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
+
+const createTokenCache = (): TokenCache => {
+  return {
+    getToken: async (key: string) => {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        if (item) {
+          console.log(`${key} was used 🔐 \n`);
+        } else {
+          console.log('No values stored under key: ' + key);
+        }
+        return item;
+      } catch (error) {
+        console.error('secure store get item error: ', error);
+        await SecureStore.deleteItemAsync(key);
+        return null;
+      }
+    },
+    saveToken: (key: string, token: string) => {
+      return SecureStore.setItemAsync(key, token);
+    },
+  };
+};
 
 export default function RootLayout() {
   return (
-    <SafeAreaView>
-      <Slot />
-    </SafeAreaView>
+    <ClerkProvider
+      publishableKey={publishableKey}
+      tokenCache={createTokenCache()}
+    >
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
